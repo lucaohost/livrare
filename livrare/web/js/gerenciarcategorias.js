843f8ddf81957cf7d23dbf3a30a516096c41946d
lucaxrl <lucas.reginatto.de.lima@gmail.com>
2018-12-07 00:22:15 -0200

    Fixing success message

--- livrare/web/js/gerenciarcategorias.js
+++ livrare/web/js/gerenciarcategorias.js
@@ -8,9 +8,7 @@ $(function () {
             }
         }).done(function (retorno) {
             if (retorno == "true" && $('#id').val() == '') {
-                swal("Sucesso!", "Aluno cadastrado com sucesso!", "success");
-            } else if (retorno == "true" && $('#id').val() != '') {
-                swal("Sucesso!", "Usuario atualizado com sucesso!", "success");
+                swal("Sucesso!", "Categoria cadastrada com sucesso!", "success");
             } else {
                 swal("Erro!", "Algo de errado aconteceu: " + retorno, "error");
             }
